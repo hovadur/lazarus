774aa5f7ec8556e14e6ec1d01c00ad66d61fa720
mattias <mattias@4005530d-fff6-0310-9dd1-cebe43e6787f>
2012-07-12 22:06:09 +0000

    clean up
    git-svn-id: http://svn.freepascal.org/svn/lazarus/tags/lazarus_1_0_RC1@37927 4005530d-fff6-0310-9dd1-cebe43e6787f

--- fixes_1_0/components/customdrawn/build_resources.sh
+++
@@ -1,1 +0,0 @@
-../../tools/lazres customdrawnimages/android.lrs customdrawnimages/android_checkbox.bmp customdrawnimages/android_checkbox_checked.bmp
--- fixes_1_0/examples/androidlcl/android/build.sh
+++
@@ -1,4 +0,0 @@
-ant debug
-~/Programas/android-sdk-linux/platform-tools/adb uninstall com.pascal.lcltest
-~/Programas/android-sdk-linux/platform-tools/adb install bin/LCLExample-debug.apk
-~/Programas/android-sdk-linux/platform-tools/adb logcat
