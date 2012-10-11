hovadur/lazarus

commit 29c2bc32d03354e4b2ddf5c2bf663ea64352176a

Gtk2: do not render listbox item by default if we are customdrawn. issue #23093
git-svn-id: http://svn.freepascal.org/svn/lazarus/trunk@39032 4005530d-fff6-0310-9dd1-cebe43e6787f

lcl/interfaces/gtk2/gtk2cellrenderer.pas
@@ -214,8 +214,24 @@ begin
 
   // draw default
   CellClass := PLCLIntfCellRendererClass(gtk_object_get_class(cell));
-  CellClass^.DefaultGtkRender(cell, Window, Widget, background_area, cell_area,
-                              expose_area, flags);
+
+  // do not call DefaultGtkRender when we are custom drawn listbox.issue #23093
+  AWinControl := nil;
+  if ColumnIndex < 0 then
+  begin
+    AWinControl := GetControl(cell, widget);
+    if [csDestroying,csLoading]*AWinControl.ComponentState<>[] then
+      AWinControl := nil;
+    if AWinControl is TCustomListbox then
+      if TCustomListbox(AWinControl).Style = lbStandard then
+        AWinControl := nil;
+    if AWinControl is TCustomCombobox then
+      AWinControl := nil;
+  end;
+  // do default draw only if we are customdrawn.
+  if (AWinControl = nil) then
+    CellClass^.DefaultGtkRender(cell, Window, Widget, background_area, cell_area,
+      expose_area, flags);
   
   if ColumnIndex < 0 then  // is a listbox or combobox
   begin

commit 64eb38377ae409598928d8fa2afe65493e3800e9

Merged revision(s) 39030, 39032-39037, 39043 from trunk: Gtk2: fixed TGtk2WSCustomListView.GetDisplayRect, also fixed cell renderer which sent wrong item id to owner drawn list view.issue #23094 ........ Gtk2: do not render listbox item by default if we are customdrawn. issue #23093 ........ Gtk2: fixed crash with TListView introduced with r39032. ........ Gtk2: LineTo and MoveToEx must take hdc offset into account.issue #23057 ........ Gtk2: fixed assertion from gtk2 library. issue #23099 ........ Gtk2: do not apply devctx offset into penPos, regression of r39034.issue #23057 ........ Gtk2: completely fixed LineTo() and MoveToEx() regressions from r39034 ........ Gtk2: fixed clientRect of GtkTreeView, new routine GetWidgetClientRect() in gtk2proc to simplify gtk2winapi, fixed offset for custom drawn listview. ........
git-svn-id: http://svn.freepascal.org/svn/lazarus/branches/fixes_1_0@39050 4005530d-fff6-0310-9dd1-cebe43e6787f

lcl/interfaces/gtk2/gtk2cellrenderer.pas
@@ -124,6 +124,7 @@ begin
       exit;
 
   ItemIndex := GetItemIndex(PLCLIntfCellRenderer(cell), Widget);
+
   if ItemIndex < 0 then
     ItemIndex := 0;
 
@@ -180,6 +181,7 @@ begin
     AreaRect := Bounds(background_area^.x, background_area^.y,
                      background_area^.Width, background_area^.Height);
 
+
     ItemIndex := GetItemIndex(PLCLIntfCellRenderer(cell), Widget);
 
     if ItemIndex < 0 then
@@ -189,7 +191,10 @@ begin
       LVTarget := dtSubItem
     else
       LVTarget := dtItem;
-    LVSubItem := ColumnIndex-1;
+    if AWinControl.FCompStyle = csListView then
+      LVSubItem := ColumnIndex
+    else
+      LVSubItem := ColumnIndex - 1;
     LVStage := cdPrePaint;
     LVState := GtkCellRendererStateToListViewDrawState(flags);
     DCWidget:=Widget;
@@ -209,8 +214,23 @@ begin
 
   // draw default
   CellClass := PLCLIntfCellRendererClass(gtk_object_get_class(cell));
-  CellClass^.DefaultGtkRender(cell, Window, Widget, background_area, cell_area,
-                              expose_area, flags);
+
+  // do not call DefaultGtkRender when we are custom drawn listbox.issue #23093
+  if ColumnIndex < 0 then
+  begin
+    AWinControl := GetControl(cell, widget);
+    if [csDestroying,csLoading,csDesigning]*AWinControl.ComponentState<>[] then
+      AWinControl := nil;
+    if AWinControl is TCustomListbox then
+      if TCustomListbox(AWinControl).Style = lbStandard then
+        AWinControl := nil;
+    if AWinControl is TCustomCombobox then
+      AWinControl := nil;
+  end;
+  // do default draw only if we are customdrawn.
+  if (ColumnIndex > -1) or ((ColumnIndex < 0) and (AWinControl = nil)) then
+    CellClass^.DefaultGtkRender(cell, Window, Widget, background_area, cell_area,
+      expose_area, flags);
   
   if ColumnIndex < 0 then  // is a listbox or combobox
   begin

lcl/interfaces/gtk2/gtk2devicecontext.inc
@@ -64,6 +64,13 @@ end;
 function TGtkDeviceContext.GetOffset: TPoint;
 var
   Fixed: Pointer;
+  AChild: PGtkWidget;
+  AColumn: PGtkTreeViewColumn;
+  Area: TGdkRectangle;
+  h: gint;
+  w: gint;
+  yoffs: gint;
+  xoffs: gint;
 begin
   Result := Point(0, 0);
   if Assigned(FWidget) then
@@ -75,6 +82,20 @@ begin
     begin
       Inc(Result.X, FWidget^.Allocation.x);
       Inc(Result.y, FWidget^.Allocation.y);
+    end;
+    if (GTK_IS_SCROLLED_WINDOW(FWidget) and GTK_IS_BIN(FWidget)) or (GTK_IS_TREE_VIEW(FWidget)) then
+    begin
+      if GTK_IS_TREE_VIEW(FWidget) then
+        AChild := FWidget
+      else
+        AChild := gtk_bin_get_child(PGtkBin(FWidget));
+      if GTK_IS_TREE_VIEW(AChild) and gtk_tree_view_get_headers_visible(PGtkTreeView(AChild)) then
+      begin
+        AColumn := gtk_tree_view_get_column(PGtkTreeView(AChild), 0);
+        gtk_tree_view_column_cell_get_size(AColumn, @Area, @xoffs, @yoffs, @w, @h);
+        // borders are 2px
+        dec(Result.y, h - 2);
+      end;
     end;
   end;
 end;

lcl/interfaces/gtk2/gtk2proc.inc
@@ -4524,6 +4524,86 @@ begin
   Result := GetWidgetOrigin(TheWidget);
 end;
 
+function GetWidgetClientRect(TheWidget: PGtkWidget): TRect;
+var
+  Widget, ClientWidget: PGtkWidget;
+  AChild: PGtkWidget;
+
+  procedure GetNoteBookClientRect(NBWidget: PGtkNotebook);
+  var
+    PageIndex: LongInt;
+    PageWidget: PGtkWidget;
+    FrameBorders: TRect;
+    aWidth: LongInt;
+    aHeight: LongInt;
+  begin
+    // get current page
+    PageIndex:=gtk_notebook_get_current_page(NBWidget);
+    if PageIndex>=0 then
+      PageWidget:=gtk_notebook_get_nth_page(NBWidget,PageIndex)
+    else
+      PageWidget:=nil;
+    if (PageWidget<>nil) and GTK_WIDGET_RC_STYLE(PageWidget)
+    and ((PageWidget^.Allocation.Width>1) or (PageWidget^.Allocation.Height>1))
+    then begin
+      // get the size of the current page
+      Result.Right:=PageWidget^.Allocation.Width;
+      Result.Bottom:=PageWidget^.Allocation.Height;
+      //DebugLn(['GetNoteBookClientRect using pagewidget: ',GetWidgetDebugReport(Widget),' ARect=',dbgs(aRect)]);
+    end else begin
+      // use defaults
+      FrameBorders:=GetStyleNotebookFrameBorders;
+      aWidth:=Widget^.allocation.width;
+      aHeight:=Widget^.allocation.height;
+      Result:=Rect(0,0,
+         Max(0,AWidth-FrameBorders.Left-FrameBorders.Right),
+         Max(0,aHeight-FrameBorders.Top-FrameBorders.Bottom));
+      //DebugLn(['GetNoteBookClientRect using defaults: ',GetWidgetDebugReport(Widget),' ARect=',dbgs(aRect),' Frame=',dbgs(FrameBorders)]);
+    end;
+  end;
+begin
+  Result := Rect(0, 0, 0, 0);
+  Widget := TheWidget;
+  ClientWidget := GetFixedWidget(Widget);
+  if (ClientWidget <> nil) then
+    Widget := ClientWidget;
+  if (Widget <> nil) then
+  begin
+    Result.Right:=Widget^.Allocation.Width;
+    Result.Bottom:=Widget^.Allocation.Height;
+    if GtkWidgetIsA(Widget,gtk_notebook_get_type) then
+      GetNoteBookClientRect(PGtkNoteBook(Widget))
+    else
+    if GTK_IS_SCROLLED_WINDOW(Widget) and GTK_IS_BIN(Widget) then
+    begin
+      AChild := gtk_bin_get_child(PGtkBin(Widget));
+      if (AChild <> nil) and GTK_IS_TREE_VIEW(AChild) then
+      begin
+        Result.Right := AChild^.allocation.width - AChild^.allocation.x;
+        Result.Bottom := AChild^.allocation.height - AChild^.allocation.y;
+      end;
+    end;
+  end;
+  {$IfDef VerboseGetClientRect}
+  if ClientWidget<>nil then begin
+    DebugLn('GetClientRect  Widget=',GetWidgetDebugReport(PgtkWidget(Handle)),
+       ' Client=',DbgS(ClientWidget),WidgetFlagsToString(ClientWidget),
+       ' WindowSize=',dbgs(Result.Right),',',dbgs(Result.Bottom),
+       ' Allocation=',dbgs(ClientWidget^.Allocation.Width),',',dbgs(ClientWidget^.Allocation.Height)
+       );
+  end else begin
+    DebugLn('GetClientRect  Widget=',GetWidgetDebugReport(PgtkWidget(Handle)),
+       ' Client=',DbgS(ClientWidget),WidgetFlagsToString(ClientWidget),
+       ' WindowSize=',dbgs(Result.Right),',',dbgs(Result.Bottom),
+       ' Allocation=',dbgs(Widget^.Allocation.Width),',',dbgs(Widget^.Allocation.Height)
+       );
+  end;
+  if GetLCLObject(Widget) is TCustomPage then begin
+    DebugLn(['TGtk2WidgetSet.GetClientRect Rect=',dbgs(Result),' ',GetWidgetDebugReport(Widget)]);
+  end;
+  {$EndIf}
+end;
+
 {-------------------------------------------------------------------------------
   TranslateGdkPointToClientArea
 

lcl/interfaces/gtk2/gtk2proc.pp
@@ -465,6 +465,7 @@ procedure UpdateNotebookTabFont(APage: TWinControl; AFont: TFont);
 // coordinate transformation
 function GetWidgetOrigin(TheWidget: PGtkWidget): TPoint;
 function GetWidgetClientOrigin(TheWidget: PGtkWidget): TPoint;
+function GetWidgetClientRect(TheWidget: PGtkWidget): TRect;
 function TranslateGdkPointToClientArea(SourceWindow: PGdkWindow;
   SourcePos: TPoint;  DestinationWidget: PGtkWidget): TPoint;
 function SubtractScoll(AWidget: PGtkWidget; APosition: TPoint): TPoint;

lcl/interfaces/gtk2/gtk2winapi.inc
@@ -4351,79 +4351,11 @@ end;
   child controls are visible.
  ------------------------------------------------------------------------------}
 function TGtk2WidgetSet.GetClientRect(handle : HWND; var ARect : TRect) : Boolean;
-var
-  Widget, ClientWidget: PGtkWidget;
-  
-  procedure GetNoteBookClientRect(NBWidget: PGtkNotebook);
-  var
-    PageIndex: LongInt;
-    PageWidget: PGtkWidget;
-    FrameBorders: TRect;
-    aWidth: LongInt;
-    aHeight: LongInt;
-  begin
-    // get current page
-    PageIndex:=gtk_notebook_get_current_page(NBWidget);
-    if PageIndex>=0 then
-      PageWidget:=gtk_notebook_get_nth_page(NBWidget,PageIndex)
-    else
-      PageWidget:=nil;
-    if (PageWidget<>nil) and GTK_WIDGET_RC_STYLE(PageWidget)
-    and ((PageWidget^.Allocation.Width>1) or (PageWidget^.Allocation.Height>1))
-    then begin
-      // get the size of the current page
-      ARect.Right:=PageWidget^.Allocation.Width;
-      ARect.Bottom:=PageWidget^.Allocation.Height;
-      //DebugLn(['GetNoteBookClientRect using pagewidget: ',GetWidgetDebugReport(Widget),' ARect=',dbgs(aRect)]);
-    end else begin
-      // use defaults
-      FrameBorders:=GetStyleNotebookFrameBorders;
-      aWidth:=Widget^.allocation.width;
-      aHeight:=Widget^.allocation.height;
-      ARect:=Rect(0,0,
-         Max(0,AWidth-FrameBorders.Left-FrameBorders.Right),
-         Max(0,aHeight-FrameBorders.Top-FrameBorders.Bottom));
-      //DebugLn(['GetNoteBookClientRect using defaults: ',GetWidgetDebugReport(Widget),' ARect=',dbgs(aRect),' Frame=',dbgs(FrameBorders)]);
-    end;
-  end;
-
 begin
   Result := false;
   if Handle = 0 then Exit;
-  ARect.Left := 0;
-  ARect.Top := 0;
-  Widget := {%H-}PGtkWidget(Handle);
-  ClientWidget := GetFixedWidget(Widget);
-  if (ClientWidget <> nil) then
-    Widget := ClientWidget;
-  if (Widget <> nil) then begin
-    ARect.Right:=Widget^.Allocation.Width;
-    ARect.Bottom:=Widget^.Allocation.Height;
-    if GtkWidgetIsA(Widget,gtk_notebook_get_type) then
-      GetNoteBookClientRect(PGtkNoteBook(Widget));
-  end else begin
-    ARect.Right:=0;
-    ARect.Bottom:=0;
-  end;
-  {$IfDef VerboseGetClientRect}
-  if ClientWidget<>nil then begin
-    DebugLn('GetClientRect  Widget=',GetWidgetDebugReport(PgtkWidget(Handle)),
-       ' Client=',DbgS(ClientWidget),WidgetFlagsToString(ClientWidget),
-       ' WindowSize=',dbgs(ARect.Right),',',dbgs(ARect.Bottom),
-       ' Allocation=',dbgs(ClientWidget^.Allocation.Width),',',dbgs(ClientWidget^.Allocation.Height)
-       );
-  end else begin
-    DebugLn('GetClientRect  Widget=',GetWidgetDebugReport(PgtkWidget(Handle)),
-       ' Client=',DbgS(ClientWidget),WidgetFlagsToString(ClientWidget),
-       ' WindowSize=',dbgs(ARect.Right),',',dbgs(ARect.Bottom),
-       ' Allocation=',dbgs(Widget^.Allocation.Width),',',dbgs(Widget^.Allocation.Height)
-       );
-  end;
-  if GetLCLObject(Widget) is TCustomPage then begin
-    DebugLn(['TGtk2WidgetSet.GetClientRect Rect=',dbgs(aRect),' ',GetWidgetDebugReport(Widget)]);
-  end;
-  {$EndIf}
-  Result:=true;
+  ARect := GetWidgetClientRect(PGtkWidget(Handle));
+  Result := True;
 end;
 
 {------------------------------------------------------------------------------
@@ -6452,9 +6384,9 @@ begin
   
   if DevCtx.IsNullPen then Exit(True);
 
-  FromPt := DevCtx.PenPos;
+  FromPt := Point(DevCtx.PenPos.X + DevCtx.Offset.X, DevCtx.PenPos.Y + DevCtx.Offset.Y);
   LPtoDP(DC, FromPt, 1);
-  ToPt := Point(X, Y);
+  ToPt := Point(X+DevCtx.Offset.X, Y+DevCtx.Offset.Y);
   LPToDP(DC, ToPt, 1);
   
   {$IFDEF DebugGDK}BeginGDKErrorTrap;{$ENDIF}
@@ -6619,15 +6551,16 @@ end;
 
  ------------------------------------------------------------------------------}
 function TGtk2WidgetSet.MoveToEx(DC: HDC; X, Y: Integer; OldPoint: PPoint): Boolean;
+var
+  DevCtx: TGtkDeviceContext absolute DC;
 begin
   Result := IsValidDC(DC);
   if Result then
-    with TGtkDeviceContext(DC) do
+    with DevCtx do
     begin
       if Assigned(OldPoint) then
         OldPoint^ := PenPos;
-
-      PenPos := Point(X, Y);
+      PenPos := Point(X, Y)
     end;
 end;
 

lcl/interfaces/gtk2/gtk2wsstdctrls.pp
@@ -1957,7 +1957,6 @@ var
   Entry: PGtkWidget;
 begin
   WidgetInfo := GetWidgetInfo({%H-}Pointer(ACustomComboBox.Handle));
-
   if gtk_is_combo_box_entry(WidgetInfo^.CoreWidget) then
   begin
     Entry := GTK_BIN(WidgetInfo^.CoreWidget)^.child;
@@ -1969,9 +1968,13 @@ begin
     else
     if (PGtkEntry(Entry)^.flag0 and $1) = Ord(NewReadOnly) then
       ReCreateCombo(ACustomCombobox, not NewReadOnly, WidgetInfo);
-  end
-  else
-    ReCreateCombo(ACustomCombobox, not NewReadOnly, WidgetInfo);
+  end else
+  begin
+    if ACustomComboBox.Style in [csOwnerDrawFixed, csOwnerDrawVariable] then
+      ReCreateCombo(ACustomCombobox, False, WidgetInfo)
+    else
+      ReCreateCombo(ACustomCombobox, not NewReadOnly, WidgetInfo);
+  end;
 end;
 
 class function TGtk2WSCustomComboBox.GetItems(